4e86d2c0aff7a3395739e0e36214f16bf5ce31b9
Vincent Perrin <vpcom01@gmail.com>
2017-12-10 19:40:26 +0100

    cleaned the first page from store demonstration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Store} from "@ngrx/store";
-import {Observable} from "rxjs/Observable";
-import {INCREMENT, DECREMENT, RESET} from './../store/home'; // TODO: import as one
 
 @Component({
   selector: 'app-home',
@@ -9,25 +6,10 @@ import {INCREMENT, DECREMENT, RESET} from './../store/home'; // TODO: import as 
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  counter$: Observable<number>;
 
-  constructor(private store: Store<any>) {
-    this.counter$ = this.store.select<any>('counter')
-  }
-
+  constructor() {  }
 
   ngOnInit() {
   }
 
-  increment() {
-    this.store.dispatch({type: 'INCREMENT'});
-  }
-
-  decrement() {
-    this.store.dispatch({type: 'DECREMENT'});
-  }
-
-  reset() {
-    this.store.dispatch({type: 'RESET'});
-  }
 }
